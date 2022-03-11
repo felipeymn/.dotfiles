da2276496ca3610e541e63f2538c8c4144f6668c
felipeymn <felipe.ymn@gmail.com>
2022-03-11 20:32:24 -0300

    feat(nvim): update lsp configs

--- .config/nvim/lua/plugins/config/lsp/init.lua
+++ .config/nvim/lua/plugins/config/lsp/init.lua
@@ -6,44 +6,28 @@ return function()
   end
 
   local null_ls = require 'plugins.config.lsp.null-ls'
-
   null_ls.setup()
   null_ls.enable_format_on_save()
 
-  local opts = { noremap=true, silent=true }
   local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-    if client.name == 'null-ls' then
+    set_lsp() -- lsp keymaps
+    if client.name ~= 'null-ls' then
       client.resolved_capabilities.document_formatting = false
       client.resolved_capabilities.document_range_formatting = false
     end
   end
-
-  local servers = { 'eslint', 'tsserver' }
 
   local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
   for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
   end
+
+  local servers = { 'eslint', 'tsserver' }
+
+  local project_library_path = '/home/felipeymn/.asdf/installs/nodejs/17.4.0/.npm/lib/node_modules/@angular/language-server/home/felipeymn/.asdf/installs/nodejs/17.4.0/.npm/lib/node_modules'
+  local angularls_path = '/home/felipeymn/.asdf/installs/nodejs/17.4.0/.npm/lib/node_modules/@angular/language-server'
+  local cmd = {"node", angularls_path, "--stdio", "--tsProbeLocations", project_library_path , "--ngProbeLocations", project_library_path}
 
   local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -57,4 +41,13 @@ return function()
       capabilities = capabilities
     }
   end
+
+  lspconfig.angularls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = cmd,
+    on_new_config = function(new_config, new_root_dir)
+      new_config.cmd = cmd
+    end,
+  }
 end
