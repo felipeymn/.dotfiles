2f04c96ee5f53cc9a51392447491e52f84d1a1ad
felipeymn <felipe.ymn@gmail.com>
2022-03-11 20:28:54 -0300

    feat(nvim): add vim options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,6 +1,7 @@
 local o = vim.opt
 local g = vim.g
 
+g.mapleader = ' '
 -- Appearance
 o.number = true
 o.signcolumn = 'yes'
@@ -11,7 +12,8 @@ o.scrolloff = 8
 o.termguicolors = true
 o.relativenumber = true
 o.colorcolumn = '80'
-
+o.cursorline = true
+o.showmode = false
 -- Backups
 o.backup = false
 o.writebackup = false
@@ -19,7 +21,8 @@ o.swapfile = false
 
 -- General
 o.errorbells = false
-
+o.mouse = 'a'
+o.shell = '/bin/bash'
 -- Indentation
 o.expandtab = true
 o.shiftwidth = 2
@@ -34,10 +37,29 @@ o.wildignore = { '.git/*', 'node_modules/*' }
 o.wildignorecase = true
 
 -- Disable builtin plugins
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
-g.loaded_zipPlugin = 1
-g.loaded_zip = 1
+local disabled_built_ins = {
+   "2html_plugin",
+   "getscript",
+   "getscriptPlugin",
+   "gzip",
+   "logipat",
+   "netrw",
+   "netrwPlugin",
+   "netrwSettings",
+   "netrwFileHandlers",
+   "matchit",
+   "tar",
+   "tarPlugin",
+   "rrhelper",
+   "spellfile_plugin",
+   "vimball",
+   "vimballPlugin",
+   "zip",
+   "zipPlugin",
+}
 
+for _, plugin in pairs(disabled_built_ins) do
+   g["loaded_" .. plugin] = 1
+end
 -- Code completion
 o.completeopt = 'menu,menuone,noselect'
