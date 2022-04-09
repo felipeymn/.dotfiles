e9a769293a4db6c70525926b6fa9d4bcbf7e2e60
felipeymn <felipe.ymm@gmail.com>
2022-04-09 11:50:22 -0300

    feat(nvim): add new options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -2,27 +2,40 @@ local o = vim.opt
 local g = vim.g
 
 g.mapleader = ' '
+
 -- Appearance
-o.number = true
-o.signcolumn = 'yes'
-o.wrap = false
-o.hlsearch = false
-o.hidden = true
-o.scrolloff = 8
-o.termguicolors = true
-o.relativenumber = true
+g.everforest_background = 'hard'
+g.everforest_better_performance = 1
+o.background= 'dark'
+o.cmdheight = 2
 o.colorcolumn = '80'
 o.cursorline = true
+o.hidden = true
+o.number = true
+o.pumheight = 10         -- popup menu height
+o.relativenumber = true
+o.scrolloff = 8
 o.showmode = false
+o.signcolumn = 'yes'
+o.termguicolors = true
+o.wrap = false
+
 -- Backups
 o.backup = false
-o.writebackup = false
 o.swapfile = false
+o.writebackup = false
 
 -- General
+o.clipboard = 'unnamedplus'
 o.errorbells = false
 o.mouse = 'a'
 o.shell = '/bin/bash'
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
+o.undofile = true   -- persistent undo
+o.updatetime = 300  -- faster completion (4000ms default)
+
 -- Indentation
 o.expandtab = true
 o.shiftwidth = 2
@@ -30,11 +43,15 @@ o.softtabstop = 2
 o.tabstop = 2
 
 -- Search
+o.hlsearch = true
 o.inccommand = 'nosplit'
 o.ignorecase = true
 o.smartcase =  true
 o.wildignore = { '.git/*', 'node_modules/*' }
 o.wildignorecase = true
+
+-- Code completion
+o.completeopt = 'menu,menuone,noselect'
 
 -- Disable builtin plugins
 local disabled_built_ins = {
@@ -61,5 +78,3 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
    g["loaded_" .. plugin] = 1
 end
--- Code completion
-o.completeopt = 'menu,menuone,noselect'
