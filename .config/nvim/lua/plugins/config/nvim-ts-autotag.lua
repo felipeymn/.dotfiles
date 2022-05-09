f56af080baed2b039fb8000ade2c3c4e86f3b60b
felipeymn <felipe.ymm@gmail.com>
2022-05-09 20:08:50 -0300

    refactor(nvim): autotag config

--- .config/nvim/lua/plugins/config/nvim-ts-autotag.lua
+++ .config/nvim/lua/plugins/config/nvim-ts-autotag.lua
@@ -1,7 +1,15 @@
+local function config()
+  local nvim_ts_autotag = safe_require 'nvim-ts-autotag'
+  if not nvim_ts_autotag then
+    return
+  end
+  nvim_ts_autotag.setup()
+end
+
 return function()
   local nvim_ts_autotag = {
     'windwp/nvim-ts-autotag',
-    config = require 'nvim-ts-autotag'.setup()
+    config = config
   }
   return nvim_ts_autotag
 end
